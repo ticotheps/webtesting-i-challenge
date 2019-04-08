822eadd159b658caa825aef6e499538f6d0fbfd0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 16:04:00 -0400

    Modifies pathway for itemsRouter import and removes dotenv import from index.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const configureMiddleware = require('./middleware.js');
-const itemsRouter = require('../users/items-router.js');
+const itemsRouter = require('../items/items-router.js');
 
 const server = express();
 
