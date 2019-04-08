c04cb3e8aa69354d0a90ad895f6c2ff79676a41f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 11:52:26 -0400

    Adds conditional statements to the succeed() function in enhancer.js to only alter the enhancement level if it is less than 20

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,16 +9,26 @@ function repair(item) {
 	if (item === undefined || item === null) {
 		return null;
 	} else if (typeof item === "object" && item.hasOwnProperty("durability")) {
-			item.durability = 100;
-			return { ...item };
+		item.durability = 100;
+		return { ...item };
   	} else {
 		return null;
 	}
 }
 
 function succeed(item) {
-	item.enhancement += 1;
-	return { ...item };
+	if (item === undefined || item === null) {
+		return null;
+	} else if (typeof item === "object" && item.hasOwnProperty("enhancement")) {
+		if (item.enhancement === 20) {
+			return item;
+		} else {
+			item.enhancement += 1;
+			return { ...item };
+		}
+  	} else {
+		return null;
+	}
 }
 
 function fail(item) {
