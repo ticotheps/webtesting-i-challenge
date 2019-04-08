89b4492953494f1fedf18337ae15115a2f1b2c19
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-08 15:53:49 -0400

    Creates a new 'items_table' using the 'npx knex migrate:make items_table' command in the terminal

---
+++ database/migrations/20190408154828_items_table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('items', items => {
+        items.increments();
+  
+        items
+            .string('name', 128)
+            .notNullable()
+            .unique();
+
+        items.integer('durability').notNullable();
+
+        items.integer('enhancement'.notNullable();
+
+        items.string('displayName', 128).notNullable();
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
